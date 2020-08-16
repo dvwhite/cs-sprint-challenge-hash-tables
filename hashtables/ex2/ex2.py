cc9e33e7b2dc7530fc2d8a83b150708008e2c69f
David White <dwhite0627@gmail.com>
2020-08-16 19:13:10 -0400

    Create a plan to solve exercise 2 and write it out as comments

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,3 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -7,8 +6,13 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     """
-    YOUR CODE HERE
+    Reconstruct a trip itinerary using hash tables
     """
-    # Your code here
+    # Create a dictionary to hold each ticket source mapped to its destination
+    # Skip the first source destination of "NONE"
+    # Use a while loop, and starting with that first source location,
+    # keep looping until the destination is equal to "NONE"
+    # On each loop, find the next destination by looking it up in the dict
+    # Return the itinerary
 
     return route

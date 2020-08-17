0793c9104e55c1bf28ea8da16693fce3bd385b52
David White <dwhite0627@gmail.com>
2020-08-16 20:25:27 -0400

    Add a solution for exercise 4 that passes all tests

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,9 +1,19 @@
 def has_negatives(a):
     """
-    YOUR CODE HERE
+    Return a list of integers in a that have a corresponding negative
+    equivalent in the list
     """
-    # Your code here
+    # Create a set for the absolute value of each number
+    # If a negative equivalent is found, it will entered in the set for
+    # that number. This will handle repeats and make it more readable
+    # than having multiple negative and positive copies of the target num
+    counts = {abs(num): set() for num in a}
+    for num in a:
+        counts[abs(num)].add(num)
 
+    # Any number with two entries (a positive and a negative) in the list
+    # would be returned in result
+    result = [num for num in counts if len(counts[num]) == 2]
     return result
 
 
